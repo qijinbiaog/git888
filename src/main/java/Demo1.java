c6d6a8d19f4b484a9c78306142a9c0135c8dc128
qb <qijinbiao1999@163.com>
2023-11-11 17:42:46 +0800

    新建

---
+++ src/main/java/Demo1.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: Demo1
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Mr.Qi
+ * @Create 2023/11/11 17:40
+ * @Version 1.0
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("新建文件");
+    }
+}
